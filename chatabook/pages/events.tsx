9df998cde33058cb263d82f180e0fa8e88a8cbe1
Maria Jakubjakova <jakubjakova.maria@gmail.com>
2023-03-17 19:50:49 +0100

    After adding, new event appears at the top, thx Michal

--- chatabook/pages/events.tsx
+++ chatabook/pages/events.tsx
@@ -1,10 +1,10 @@
 import CreateEventForm from '@/components/CreateEventForm';
-import { gql, useQuery } from '@apollo/client';
+import { gql, NetworkStatus, useQuery } from '@apollo/client';
 import type { Event, PrismaClient, Prisma } from '@prisma/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import EventTile from '@/components/EventTile';
 
-type EventWithUser = Prisma.EventGetPayload<{
+export type EventWithUser = Prisma.EventGetPayload<{
   include: {
     user: true;
   };
@@ -30,16 +30,21 @@ const AllEventsQuery = gql`
 `;
 
 export default function Events() {
-  const { data, loading, error } = useQuery(AllEventsQuery);
+  const [allEvents, setAllEvents] = useState<EventWithUser[] | null>(null);
+  const { data, loading, error, networkStatus } = useQuery(AllEventsQuery, {
+    onCompleted: (data) => setAllEvents(data.events),
+    notifyOnNetworkStatusChange: true,
+  });
   const [showForm, setShowForm] = useState(false);
 
   const handleClick = () => {
     setShowForm(!showForm);
   };
 
+  if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
-  console.log(data);
+  console.log(allEvents);
 
   return (
     <>
@@ -47,23 +52,24 @@ export default function Events() {
         <button className="border-black rounded-md border-solid border-[3px]" onClick={handleClick}>
           Vytvorit udalost
         </button>
-        {showForm && <CreateEventForm />}
+        {showForm && <CreateEventForm getUpdatedData={setAllEvents} showForm={setShowForm} />}
       </div>
-      {data.events.map((event: EventWithUser) => (
-        <EventTile
-          id={event.id}
-          key={event.id}
-          userId={event.userId}
-          name={event.user.name}
-          occassion={event.occassion}
-          start={event.start}
-          end={event.end}
-          message={event.message}
-          people={event.people}
-          appartments={event.appartments}
-          whole={event.whole}
-        />
-      ))}
+      {allEvents &&
+        allEvents.map((event: EventWithUser) => (
+          <EventTile
+            id={event.id}
+            key={event.id}
+            userId={event.userId}
+            name={event.user.name}
+            occassion={event.occassion}
+            start={event.start}
+            end={event.end}
+            message={event.message}
+            people={event.people}
+            appartments={event.appartments}
+            whole={event.whole}
+          />
+        ))}
     </>
   );
 }
