bb53cb51a75b31a12116a9904456110dfd3ad407
Maria Jakubjakova <jakubjakova.maria@gmail.com>
2023-06-30 11:11:59 +0200

    prisma client imported

--- chatabook/graphql/resolvers.tsx
+++ chatabook/graphql/resolvers.tsx
@@ -2,7 +2,9 @@ import { EventWithUser } from '@/pages/events';
 import { createDate } from '@/scripts/createDate';
 import { Event, User, User_role } from '@prisma/client';
 import validator from 'validator';
-import prisma from '@/lib/prisma';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export const resolvers = {
   Query: {
